a695d3539fe8e6ecd6b888c5657f65b2dd4b6ffd
dundunHa <132465056+dundunHa@users.noreply.github.com>
2024-06-06 23:39:31 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -1,21 +1,27 @@
-export async function onRequestPost(context) {  // Contents of context object  
+export async function onRequestPost(context) {  
     const {   
-        request, // same as existing Worker API    
-    env, // same as existing Worker API    
-    params, // if filename includes [id] or [[path]]   
-     waitUntil, // same as ctx.waitUntil in existing Worker API    
-     next, // used for middleware or to fetch assets    
-     data, // arbitrary space for passing data between middlewares 
-     } = context;
-     context.request
-     const url = new URL(request.url);
-     const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
-         method: request.method,
-         headers: request.headers,
-         body: request.body,
-     });
-    console.log("upload function")
-    console.log(response)
-    return response;
-  }
-  
+        request, 
+        env, 
+        params, 
+        waitUntil, 
+        next, 
+        data,
+    } = context;
+
+    const url = new URL(request.url);
+    const response = await fetch('https://telegra.ph/' + url.pathname + url.search, {
+        method: request.method,
+        headers: request.headers,
+        body: request.body,
+    });
+    const modifiedResponse = new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: {
+            ...response.headers,
+            'Access-Control-Allow-Origin': '*',
+        },
+    });
+
+    return modifiedResponse;
+}
